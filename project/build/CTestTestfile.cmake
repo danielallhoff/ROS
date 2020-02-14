d96c3a29e49e0316f6553e802b66e6ada02627f5
AlfonsoParraga <apm223@alu.ua.es>
2020-02-14 18:14:45 +0100

    Workspace

---
+++ project/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/alfonso/ROS/project/src
+# Build directory: /home/alfonso/ROS/project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
