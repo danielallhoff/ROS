7d5caadc3dff40547bae852b1fca7dfd17f9a5ad
AlfonsoParraga <apm223@alu.ua.es>
2020-02-15 11:28:20 +0100

    Borrado build

--- project/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniel/ROS/project/src
-# Build directory: /home/daniel/ROS/project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
