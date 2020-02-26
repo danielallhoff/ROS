dca88802a31ef764244e7d540de13098cdaf693a
AlfonsoParraga <apm223@alu.ua.es>
2020-02-26 09:38:31 +0100

    Gatherer saving only command

--- project/src/gatherer/src/gatherer.py
+++ project/src/gatherer/src/gatherer.py
@@ -14,35 +14,23 @@ print('Saving path = {}'.format(PATH))
 
 class Gatherer:
     def __init__(self):
-        #self.image_sub = rospy.Subscriber('/robot1/camera/rgb/image_raw', Image, self.Gather, queue_size = 1, buff_size = 2**30)
         self.frame = 0
         self.bridge = CvBridge()
         self.cmd_sub = rospy.Subscriber('cmd_key', String, self.save_key, queue_size = 1)
         self.image_sub = rospy.Subscriber('image', Image, self.save_image, queue_size = 1)
 
-    def SaveLine(self, string):
-        string = string + '\n'
-        with open('command_keys.txt', 'a') as out:
-            out.write(string)
-
     def save_key(self, command):
-        cadena = str(command) + str('\n')
+        ''' Saves command key into file'''
+        cadena = str(command)
         with open('command_keys.txt', 'a') as out:
-            out.write(cadena)
+            splitted = cadena.split("\"")
+            out.write(splitted[1] + str('\n'))
 
     def save_image(self, data):
+        ''' Saves each image recieved'''
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")   
         cv.imwrite(PATH + "_frame_" + str(self.frame) + ".jpg", cv_image)   
         self.frame += 1
-
-    def Gather(self, command):
-        data = rospy.wait_for_message("image", Image)
-        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")            
-        cv.imshow("view", cv_image)
-        cv.imwrite(PATH + "frame_" + str(self.frame) + "_" + command + ".jpg", cv_image)   
-        self.frame += 1
-        SaveLine()
-
 
 def main():
     handler = Gatherer()
