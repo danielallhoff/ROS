2e9621910111a1270e8e290df1c2c0b6f6a2e698
AlfonsoParraga <apm223@alu.ua.es>
2020-02-28 20:04:48 +0100

    Uso de message_filters en gatherer
    Co-authored-by: danielallhoff <32059598+danielallhoff@users.noreply.github.com>

--- project/src/gatherer/src/gatherer.py
+++ project/src/gatherer/src/gatherer.py
@@ -7,6 +7,7 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
+import message_filters
 
 PATH = os.path.abspath('images')
 PATH = PATH + "/images"
@@ -16,8 +17,31 @@ class Gatherer:
     def __init__(self):
         self.frame = 0
         self.bridge = CvBridge()
-        self.cmd_sub = rospy.Subscriber('cmd_key', String, self.save_key, queue_size = 1)
-        self.image_sub = rospy.Subscriber('image', Image, self.save_image, queue_size = 1)
+        self.dir_image_sub = message_filters.Subscriber('/robot1/camera/rgb/image_raw', Image)
+        self.vel_sub = message_filters.Subscriber('/robot1/mobile_base/commands/velocity', Twist)
+        self.timeSync = message_filters.ApproximateTimeSynchronizer([self.dir_image_sub, self.vel_sub], queue_size=10,slop=0.1, allow_headerless=True)
+        self.timeSync.registerCallback(self.SyncCallback)
+        #self.cmd_sub = rospy.Subscriber('cmd_key', String, self.save_key, queue_size = 1)
+        #self.image_sub = rospy.Subscriber('image', Image, self.save_image, queue_size = 1)
+        self.angular_velocity = 0
+        self.linear_velocity = 0
+
+    def SyncCallback(self, image, velocity):
+        self.save_velocity(velocity)
+        self.save_image(image)
+
+
+    def save_velocity(self, twist):
+        '''Saves velocities linear and angular into vel.txt'''
+        cadena = str(twist.linear.x) + ';' + str(twist.angular.z) + ';'
+        with open('vel.txt', 'a') as out:
+            out.write(cadena + str('\n'))
+
+    def callback(self, data):
+        cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        cv.imwrite(PATH + "_frame_" + str(self.frame) + ".jpg", cv_image)   
+        self.frame += 1
+        self.save_velocity()
 
     def save_key(self, command):
         ''' Saves command key into file'''
--- project/src/keyboard_reader/src/keyboard_reader.py
+++ project/src/keyboard_reader/src/keyboard_reader.py
@@ -10,74 +10,71 @@ from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 
-class KeyBoardHandler:
-    def __init__(self):
-        
-        pubDest = '/robot1/mobile_base/commands/velocity'
-        #pubDest = 'cmd_vel'
-        self.cmd_vel_pub = rospy.Publisher(pubDest,Twist, queue_size=1)
-        self.frame = 0
-        self.cmd_key_pub = rospy.Publisher('cmd_key',String, queue_size=1)
-        self.last_key = 'n'
+command_message = """
+w/s : increase/decrease only linear speed by 10%
+a/d : increase/decrease only angular speed by 10%
 
-    def driveKeyboard(self):
-        twist = Twist()
-        cancel_future_calls = self.call_repeatedly(1, self.Timeout)
-        print('Before loop')
-        while(1):
-            print('Another loop iteration')
-            cmd_line = raw_input("Type a command and then press enter. Use '+' to move forward, 'm' move backward, 'l' to turn left, 'r' to turn right, '.' exit ")
-            if cmd_line:
-                char = cmd_line[0]
-                self.last_key = char
-                if((char != '+') and (char != 'l') and (char != 'r') and (char != '.') and (char != 'm')):
-                    print("Unknown command " + cmd_line)
-                    self.last_key = 'n'
+. to quit
+"""
 
-                twist.linear.x = 0
-                twist.linear.y = 0
-                twist.linear.z = 0
-                twist.angular.x = 0
-                twist.angular.y = 0
-                twist.angular.z = 0
-
-                # Salida red -> 0
-                if char == '+':
-                    twist.linear.x = 0.50
-                # Salida red -> 1
-                elif char == 'l':
-                    twist.angular.z = 0.75
-                    twist.linear.x = 0.25
-                # Salida red -> 2
-                elif char == 'r':
-                    twist.angular.z = -0.75
-                    twist.linear.x = 0.25
-                # Exit
-                elif char == '.':
-                    cancel_future_calls()
-                    break
-                # Salida red -> 3
-                elif char == 'm':
-                    twist.linear.x = -0.50
-                                
-                self.cmd_vel_pub.publish(twist)
-
-            else:
-                print("Empty command")
-
-
-    def call_repeatedly(self, interval, func, *args):
+def call_repeatedly(interval, func, *args):
         stopped = Event()
         def loop():
             while not stopped.wait(interval): # the first call is in "interval" secs
                 func(*args)
         Thread(target=loop).start()    
         return stopped.set
-  
+
+class KeyBoardHandler:
+    def __init__(self):
+        
+        pubDest = '/robot1/mobile_base/commands/velocity'
+        #pubDest = 'cmd_vel'
+        self.cmd_vel_pub = rospy.Publisher(pubDest, Twist, queue_size=1)
+        self.frame = 0
+        self.cmd_key_pub = rospy.Publisher('cmd_key', String, queue_size=1)
+        self.last_key = 'n'
+
+    def driveKeyboard(self):
+        twist = Twist()
+        twist.linear.x = 0.5
+        #cancel_future_calls = call_repeatedly(1, self.Timeout)
+        while(1):
+            cmd_line = raw_input(command_message)
+            if cmd_line:
+                char = cmd_line[0]
+                self.last_key = char
+            else:
+                char = self.last_key
+
+            if((char != 'w') and (char != 'a') and (char != 's') and (char != 'd') and (char != '.')):
+                char = 'n'
+
+            # Salida red -> 0
+            if char == 'w':
+                twist.angular.z = 0
+                twist.linear.x = twist.linear.x * 1.1
+            # Salida red -> 1
+            elif char == 'a':
+                twist.angular.z = 0.15
+                twist.linear.x = twist.linear.x * 0.8
+            # Salida red -> 2
+            elif char == 's':
+                twist.angular.z = 0
+                twist.linear.x = twist.linear.x * 0.9
+            # Salida red -> 3
+            elif char == 'd':
+                twist.angular.z = -0.15
+                twist.linear.x = twist.linear.x * 0.8
+            # Exit
+            elif char == '.':
+                #cancel_future_calls()
+                break
+                            
+            self.cmd_vel_pub.publish(twist)
+
     def Timeout(self):
         self.cmd_key_pub.publish(self.last_key)
-        self.last_key = 'n'
-        print('Timeout')
 
 
 def main():
