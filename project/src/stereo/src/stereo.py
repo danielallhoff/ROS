7d93c95469acce30100b0b2868a8b3266c9623a0
AlfonsoParraga <apm223@alu.ua.es>
2020-03-22 18:59:44 +0100

    Fix fails

--- project/src/stereo/src/stereo.py
+++ project/src/stereo/src/stereo.py
@@ -1,48 +1,68 @@
+#!/usr/bin/env python
+
 import rospy
+import numpy as np
 import cv2 as cv
 import sys
 import os
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import yaml
+#Markers for points visualization: https://answers.ros.org/question/231781/rviz-markers-with-rospy/
+from sensor_msgs.msg import PointCloud
+import message_filters
 
 PATH_SYNC = os.path.abspath('images_stereo')
-print('Saving path = {}'.format(PATH))
-PATH_
+print('Saving path = {}'.format(PATH_SYNC))
+
 #Right: /robot1/trasera1
 #Left: /robot1/trasera2
 #Ros calibration node: http://wiki.ros.org/camera_calibration/Tutorials/StereoCalibration
 class Stereo:
-    __init__(self):
-        self.sub_trasR = rospy.Subscriber("/robot1/trasera1/trasera1/rbg/image_raw")
-        self.sub_trasL = rospy.Subscriber("/robot1/trasera2/trasera2/rbg/image_raw")
+    def __init__(self):
+        self.sub_trasR = message_filters.Subscriber("/robot1/trasera1/trasera1/rbg/image_raw", Image)
+        self.sub_trasL = message_filters.Subscriber("/robot1/trasera2/trasera2/rbg/image_raw", Image)
         self.timeSync = message_filters.ApproximateTimeSynchronizer([self.sub_trasL, self.sub_trasR], queue_size=10,slop=0.1, allow_headerless=True)
         self.timeSync.registerCallback(self.sync)
+        self.height, self.width, self.channel = 640, 480, 3
         self.load_params()
         self.bridge = CvBridge()
-        height, width, channel = 640, 480, 3
+        self.stereo_cloud_publisher = rospy.Publisher("robot1/stereo_cloud", PointCloud, queue_size=10)
     
-    def load_params():
+    def load_params(self):
         #Camera matrix
-        self.Kl = [ [1086.56956,     0.     ,   359.39663],
+        self.Kl = np.float32([ [1086.56956,     0.     ,   359.39663],
              [0.     ,   830.34232,   227.63702],
-             [0.     ,     0.     ,     1.     ]]
+             [0.     ,     0.     ,     1.     ]])
         #Distortion
-        self.distl = [0.095862, 1.267249, 0.002377, 0.000967, 0.000000]
+        self.distl = np.float32([0.095862, 1.267249, 0.002377, 0.000967, 0.000000])
         
-        self.Kr = [ [1029.15929,     0.     ,   409.73459],
+        self.Kr = np.float32([ [1029.15929,     0.     ,   409.73459],
              [0.     ,   779.47028,   291.03151],
-             [0.     ,     0.     ,     1.     ]]
-        self.distr = [0.159904, 0.099552, 0.045093, 0.026736, 0.000000]
+             [0.     ,     0.     ,     1.     ]])
+        self.distr = np.float32([0.159904, 0.099552, 0.045093, 0.026736, 0.000000])
 
-        newcameramtxL, roiL = cv.getOptimalNewCameraMatrix(self.Kl, self.distl, (width, height), 1, (width,height))
-        newcameramtxR, roiR = cv.getOptimalNewCameraMatrix(self.Kr, self.distr, (width, height), 1, (width,height))
+        self.newcameramtxL, self.roiL = cv.getOptimalNewCameraMatrix(self.Kl, self.distl, (self.width, self.height), 1, (self.width, self.height))
+        self.newcameramtxR, self.roiR = cv.getOptimalNewCameraMatrix(self.Kr, self.distr, (self.width, self.height), 1, (self.width, self.height))
 
     #Output cloud to rviz or somewhere
-    def output(verts, colors):
+    def output(self, verts, colors):
+        pointcloud = PointCloud()
+        header = std_msgs.msg.Header()
+        header.stamp = rospy.Time.now()
+        #Use frame_id of the sensor
+        header.frame_id = "pcl"
+        pointcloud.header = header
+
         verts = verts.reshape(-1,3)
         colors = colors.reshape(-1,3)
+
+        for i in range(0,len(verts)):
+            pointcloud.points[i].x = verts[i][0]
+            pointcloud.points[i].y = verts[i][1]
+            pointcloud.points[i].z = verts[i][2]
+        
+        self.stereo_cloud_publisher.publish(pointcloud)
 
 
     #Search for K, R and t optimised
@@ -52,32 +72,38 @@ class Stereo:
         #imageR = cv.pyrDown(self.bridge.imgmsg_to_cv2(imageR, "bgr8"))
         
         #Rectify images with params of camera matrix(K) and distortion for each side of camera
-        imageL = cv.undistort(imageL, self.Kl, self.distl, None, newcameramtxL)
-        imageR = cv.undistort(imageR, self.Kr, self.distr, None, newcameramtxR)
+        imageL = cv.undistort(imageL, self.Kl, self.distl, None, self.newcameramtxL)
+        imageR = cv.undistort(imageR, self.Kr, self.distr, None, self.newcameramtxR)
         #Calculate disparity image
         stereo = cv.createStereoBM(numDisparities=16, blockSize=15)
         disparity = stereo.compute(imageL, imageR)
     
-        f = 0.8 * width
+        f = 0.8 * self.width
 
-        Q = np.float32([[1, 0, 0, -0.5*width],
-                [0,-1, 0,  0.5*height], # turn points 180 deg around x-axis,
+        Q = np.float32([[1, 0, 0, -0.5*self.width],
+                [0,-1, 0,  0.5*self.height], # turn points 180 deg around x-axis,
                 [0, 0, 0,     -f], # so that y-axis looks up
                 [0, 0, 1,      0]])
         #Obtain points
         points3d = cv.reprojectImageTo3D(disparity, Q)
         #Obtain colors
-        colors3d = cv.cvtColor(imgL, cv.COLOR_BGR2RGB)
+        colors3d = cv.cvtColor(imageL, cv.COLOR_BGR2RGB)
         #Apply mask
-        mask = disp > disp.min()
-        out_points = points[mask]
-        out_colors = colors[mask]
-        cv.imshow('left', imgL)
-        cv.imshow('disparity', (disp-min_disp)/num_disp)
+        mask = disparity > disparity.min()
+        out_points = points3d[mask]
+        out_colors = colors3d[mask]
+        #cv.imshow('left', imageL)
+        #cv.imshow('disparity', (disparity-min_disp)/num_disp)
         self.output(out_points, out_colors)
+
 if __name__ == '__main__':
     rospy.init_node('StereoHandler', anonymous=True)
-    handler = Stereo()    
+    handler = Stereo()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
 #https://github.com/opencv/opencv/blob/master/samples/python/stereo_match.py
 
 
