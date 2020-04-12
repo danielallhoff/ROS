5b7ff135f2295d2d20d2b7aecf32140d0003330e
AlfonsoParraga <apm223@alu.ua.es>
2020-04-12 18:49:27 +0200

    Últimos cambios trabajados en local

--- project/src/robot_detection_handler/src/robot_detection.py
+++ project/src/robot_detection_handler/src/robot_detection.py
@@ -4,12 +4,13 @@ import rospy
 import cv2 as cv
 import sys
 import os
+import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 from keras import backend as K
-from keras.models import Model, load_model
+from tensorflow.keras.models import load_model
 PATH = os.path.abspath('src')
 print('Loading path = {}'.format(PATH))
 
@@ -23,9 +24,9 @@ class RobotDetectionHandler:
         #self.image_pub = rospy.Publisher("image_topic_2", Image, queue_size=5)
         #Subscribe to camera of robot and receive data
         self.image_sub = rospy.Subscriber('/robot1/camera/rgb/image_raw', Image, self.callback, queue_size = 1)
-        self.model = load_model(PATH+"/model_trained.h5")
+        self.model = load_model(PATH+"/mobilenetssd.h5")
         #pubDest = 'cmd_vel'
-       
+        self.model.summary()
         print("Init")
 
     def callback(self, data):
@@ -34,16 +35,18 @@ class RobotDetectionHandler:
         try:
             #For receive key            
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")            
-            
+            resized = cv.resize(cv_image, dsize=(IMG_HEIGHT, IMG_WIDTH), interpolation=cv.INTER_CUBIC)
+            image = np.asarray(resized).astype('float32')/255.0
             (rows, cols, channels) = cv_image.shape
+            array = np.array(image)
+            array = array.reshape((1,) + array.shape)
             #Image size to big downsize
-            if cols > IMG_WIDTH and rows > IMG_HEIGHT :
-                cv.resize(cv_image, (IMG_HEIGHT,IMG_WIDTH))
             #Res 0-xmin 1-ymin 2-xmax 3-ymax
-            res = self.model.predict(cv_image)
-            cv.Poin
-            cv.rectangle(cv_image, (res[0], res[1]), (res[2], res[3]), thickness = 2)
-            imshow("Detection", cv_image)
+            res = self.model.predict(array)
+            
+            cv_image = cv.rectangle(resized, (res[0][0], res[0][1]), (res[0][2], res[0][3]), color=(0,255,0),thickness = 2)
+            print(res)
+            cv.imwrite(PATH + "/detection.jpg", cv_image)
         except Exception as e:
             print(e)
         
--- project/src/robot_movement_handler/src/robot_movement.py
+++ project/src/robot_movement_handler/src/robot_movement.py
@@ -4,17 +4,21 @@ import rospy
 import cv2 as cv
 import sys
 import os
+import numpy as np
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 from keras import backend as K
-from keras.models import Model, load_model
+#from keras.models import Model
+from tensorflow.keras.models import load_model
+from keras.preprocessing.sequence import TimeseriesGenerator
 PATH = os.path.abspath('src')
 print('Loading path = {}'.format(PATH))
 
-IMG_HEIGHT = 28
-IMG_WIDTH = 28
+IMG_HEIGHT = 48
+IMG_WIDTH = 48
+SEQ_LENGTH = 1
 
 class RobotHandler:
     def __init__(self):
@@ -23,31 +27,39 @@ class RobotHandler:
         #self.image_pub = rospy.Publisher("image_topic_2", Image, queue_size=5)
         #Subscribe to camera of robot and receive data
         self.image_sub = rospy.Subscriber('/robot1/camera/rgb/image_raw', Image, self.callback, queue_size = 1)
-        self.model = load_model(PATH+"/model_trained.h5")
+        self.model = load_model(PATH+"/resnet_trained.h5")
         #pubDest = 'cmd_vel'
         self.cmd_vel_pub = rospy.Publisher('/robot1/mobile_base/commands/velocity', Twist, queue_size=1)
         print("Init")
+        self.current_data = []
 
     def callback(self, data):
-        #Obtain images
-        print("Callback call")
         try:
             #For receive key            
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")            
             
             (rows, cols, channels) = cv_image.shape
             #Image size to big downsize
-            if cols > IMG_WIDTH and rows > IMG_HEIGHT :
-                cv.circle(cv_image, (IMG_HEIGHT,IMG_WIDTH), 10, 255)
+            resized = cv.resize(cv_image, dsize=(IMG_HEIGHT, IMG_WIDTH), interpolation=cv.INTER_CUBIC)
+            image = np.asarray(resized).astype('float32')/255.0
             
-            res = self.model.predict(cv_image)
+            self.current_data.append(image)
+            if len(self.current_data) < SEQ_LENGTH:
+                return
+
+            array = np.array(self.current_data)
+            #array = array.reshape((1,) + array.shape)
+            res = self.model.predict(array)
+            print(res)
             twist = Twist()
-            twist.linear.x = res[0]
-            twist.angular.z = res[1]
+            twist.linear.x = res[0][0]
+            twist.angular.z = res[0][1]
             
             self.cmd_vel_pub.publish(twist)
+            self.current_data.pop(0)
         except Exception as e:
             print(e)
+            self.current_data.pop(0)
         
 def main():
     print("Start")
